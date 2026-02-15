48238aec060f2dcbf2118bc5294e7a4bee9d1204
cyrozap <cyrozap@gmail.com>
2026-02-14 23:29:28 -0600

    s/library/module/

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 /*
- *  Parser library for GenCAD files.
+ *  Parser module for GenCAD files.
  *  Copyright (C) 2026  Forest Crossman <cyrozap@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
